6f5757c8acb7cc0ee9324d2676438b5deead7b7a
Michal Grzelak <michal.grzelak@stxnext.pl>
2019-04-18 11:41:12 +0200

    show rendering

--- src/MenuButton.js
+++ src/MenuButton.js
@@ -4,8 +4,8 @@ import "./MenuButton.css";
 class MenuButton extends Component {
   render() {
     return (
-      <button id="roundButton" 
-              onMouseDown={this.props.handleMouseDown}></button>
+      <button id="roundButton"
+    onMouseDown={this.props.handleMouseDown}/>
     );
   }
 }
